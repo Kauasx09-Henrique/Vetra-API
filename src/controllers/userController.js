bb3bd7e946f37dee1175c268bfc92a5b67b24ce9
Kauã_Henrique_Dev <166959910+Kauasx09-Henrique@users.noreply.github.com>
2026-02-23 14:35:59 -0300

    Add function to update user status

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -52,4 +52,26 @@ const deleteUser = async (req, res) => {
     }
 };
 
-module.exports = { getAllUsers, deleteUser };
+//atualizar status do usuário (usuario comum/admin) - OPÇÃO PARA FUTURAS IMPLEMENTAÇÕES
+
+const atualizarStatusUsuario = async (req, res) => {
+    const { id } = req.params;
+    const { tipo } = req.body; // 'ADMIN' ou 'CLIENTE'
+    if (!['ADMIN', 'CLIENTE'].includes(tipo)) {
+        return res.status(400).json({ error: 'Tipo de usuário inválido. Use "ADMIN" ou "CLIENTE".' });
+    }
+
+    try {
+        const userCheck = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
+        if (userCheck.rows.length === 0) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+        await pool.query('UPDATE usuarios SET tipo = $1 WHERE id = $2', [tipo, id]);
+        return res.status(200).json({ message: `Tipo de usuário atualizado para ${tipo}.` });
+    } catch (err) {
+        console.error('Erro ao atualizar tipo de usuário:', err);
+        return res.status(500).json({ error: 'Erro interno ao atualizar tipo de usuário.' });
+    }
+}
+
+module.exports = { getAllUsers, deleteUser, atualizarStatusUsuario };
